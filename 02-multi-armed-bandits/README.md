ce4fe99619d8d2757d267914d094f235d720b801
bosak <bosakmaw@gmail.com>
2019-02-14 01:08:09 +0200

    add agent subclasses and non-stationary problem

--- 02-multi-armed-bandits/README.md
+++ 02-multi-armed-bandits/README.md
@@ -1,4 +1,18 @@
-# Experiments with different ε values
-Tests are made for ε = `0.1`, `0.01`, `0.001` and `0`
-![total rewards](results/total_rewards.png)
-![average rewards](results/average_rewards.png)
+# Experiments for Multi-Armed bandits
+
+## Stationary problem
+
+Tests are made with action value sample-average agents for ε = `0.1`, `0.01`, `0.001` and `0`
+![stationary total rewards](results/stationary_total_rewards.png)
+![stationary average rewards](results/stationary_average_rewards.png)
+
+## Non-stationary problem
+
+Tests made with two agents:
+ - Agent with standard sample-average action value with ε = `0.1`
+ - Agent with fixed step-size α = `0.1` and ε = `0.1`
+
+![nonstationary total rewards](results/nonstationary_total_rewards.png)
+![nonstationary average rewards](results/nonstationary_average_rewards.png)
+
+We can see that the fixed-step agent performed better for a non-stationary problem.
--- 02-multi-armed-bandits/bandits.py
+++ 02-multi-armed-bandits/bandits.py
@@ -4,14 +4,13 @@ import numpy as np
 import seaborn as sns
 from scipy.stats import norm
 from tqdm import tqdm
+from abc import abstractmethod
 
 BANDITS = 10
-MIN_BANDITS = 5
-MAX_BANDITS = 15
 PROBLEMS = 2000
 STEPS = 1000
 
-def generate(normal, n=BANDITS, plot=False):
+def generate_stationary(normal, n=BANDITS, plot=False):
     means = np.random.randn(n)
     data = np.repeat(normal[:, np.newaxis], n, axis=1) + means
 
@@ -27,13 +26,21 @@ def generate(normal, n=BANDITS, plot=False):
 
     return means
 
+def generate_nonstationary(n=BANDITS):
+    mean = np.random.randn()
+    means = np.full(n, mean)
+
+    return means
+
+def update_nonstationary(means):
+    means += np.random.normal(scale=0.01, size=means.shape)
+
 class Agent(object):
     def __init__(self, eps):
         self.eps = eps
     
     def init(self, actions):
         self.actions = actions
-        self.n = np.zeros(actions)
         self.q = np.zeros(actions)
     
     def act(self):
@@ -42,29 +49,69 @@ class Agent(object):
         else:
             return np.random.choice(self.actions)
     
+    @abstractmethod
+    def step_size(self, a):
+        pass
+
+    def update(self, a, r):
+        self.q[a] += self.step_size(a) * (r - self.q[a])
+
+    @abstractmethod
+    def legend(self):
+        pass
+
+class ActionValueAgent(Agent):
+    def __init__(self, eps):
+        super().__init__(eps)
+    
+    def init(self, actions):
+        super().init(actions)
+        self.n = np.zeros(actions)
+
+    def step_size(self, a):
+        return 1 / self.n[a]
+    
     def update(self, a, r):
         self.n[a] += 1
-        self.q[a] += 1/self.n[a] * (r - self.q[a])
+        super().update(a, r)
+    
+    def legend(self):
+        return 'ε = {}, Action Value'.format(self.eps)
 
-def plot_rewards(metric, title, eps):
+class FixedStepAgent(Agent):
+    def __init__(self, eps, alpha):
+        super().__init__(eps)
+        self.alpha = alpha
+
+    def step_size(self, a):
+        return self.alpha
+    
+    def legend(self):
+        return 'ε = {}, α = {}, Fixed Step'.format(self.eps, self.alpha)
+
+
+def plot_rewards(metric, title, xlim, legend):
     plt.plot(metric)
     plt.title(title)
     plt.gcf().canvas.set_window_title(title)
-    plt.xlim(0, STEPS)
+    plt.xlim(0, xlim)
     plt.xlabel('Step')
     plt.ylabel('Reward')
-    plt.gca().legend(tuple('ε = {}'.format(e) for e in eps))
+    plt.gca().legend(legend)
     plt.savefig('results/{}.png'.format(title.lower().replace(' ', '_')))
     plt.show()
 
-def main(problems=PROBLEMS, steps=STEPS):
+def main(problems=PROBLEMS, steps=STEPS, stationary=True):
     eps = [0.1, 0.01, 0.001, 0]
     normal = np.random.randn(10000)
-    agents = [Agent(eps=e) for e in eps]
+    agents = [ActionValueAgent(eps=0.1), FixedStepAgent(eps=0.1, alpha=0.1)]
     total_rewards = np.zeros((steps, len(agents)))
     for p in tqdm(range(problems)):
-        bandits = BANDITS #np.random.randint(MIN_BANDITS, MAX_BANDITS + 1)
-        means = generate(normal, bandits, plot=False)
+        bandits = BANDITS
+        if stationary:
+            means = generate_stationary(normal, bandits, plot=False)
+        else:
+            means = generate_nonstationary(bandits)
         bandit = lambda a: np.random.randn() + means[a]
         for agent in agents:
             agent.init(bandits)
@@ -74,10 +121,13 @@ def main(problems=PROBLEMS, steps=STEPS):
                 r = bandit(a)
                 agent.update(a, r)
                 total_rewards[t, i] += r
+            if not stationary:
+                update_nonstationary(means)
     
     average_rewards = total_rewards / problems
-    plot_rewards(total_rewards, 'Total Rewards', eps)
-    plot_rewards(average_rewards, 'Average Rewards', eps)
+    legend = tuple(agent.legend() for agent in agents)
+    plot_rewards(total_rewards, 'Total Rewards', steps, legend)
+    plot_rewards(average_rewards, 'Average Rewards', steps, legend)
 
 if __name__ == '__main__':
-    main(problems=PROBLEMS)
+    main(stationary=False, problems=PROBLEMS, steps=10_000)
